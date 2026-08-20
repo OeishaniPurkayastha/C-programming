37efa6f1cf4d9484ccf2af7c5281b84099e0dade
OeishaniPurkayastha <oeishanipurkayastha@gmail.com>
2026-08-20 22:40:34 +0530

    Add concept of space program

---
+++ concept_of_space.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    float a = 6.32;
+    float b = 3.14;
+
+    printf("Floating point 1: %8.2f\n", a);
+    printf("Floating point 2: %-18.2f\n", b);
+
+    return 0;
+}
