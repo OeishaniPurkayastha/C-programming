e812ce361001398b0468da4c3b9854c5520b3e0e
OeishaniPurkayastha <oeishanipurkayastha@gmail.com>
2026-08-20 21:35:18 +0530

    Add a format specifier program

---
+++ format_specifiers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num = 42;
+    float pi = 3.14159;
+    char letter = 'A';
+
+    // Using format specifiers to print different types of variables
+    printf("Integer: %d\n", num);
+    printf("Floating point: %.2f\n", pi);
+    printf("Character: %c\n", letter);
+
+    return 0;
+}
